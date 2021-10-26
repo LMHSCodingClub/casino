6a02b5f1a451fef218bf93274e48da098eaf1e02
mshvartsberg <meggan567@gmail.com>
2021-10-26 15:03:10 -0400

    Add commented code

--- src/main/java/com/lmhscodingclub/casino/slots/Slots.java
+++ src/main/java/com/lmhscodingclub/casino/slots/Slots.java
@@ -14,3 +14,36 @@ public class Slots {
         slotGame.play();
     }
 }
+
+
+/*
+import java.io.*;
+import static java.lang.System.*;
+
+import java.util.Scanner;
+import java.lang.Math;
+
+class Main{
+
+     public static void main (String str[]) throws IOException {
+         int slot1 = (int)(Math.random()*(20)+1);
+         int slot2 = (int)(Math.random()*(20)+1);
+         int slot3 = (int)(Math.random()*(20)+1);
+         System.out.println(slot1);
+         System.out.println(slot2);
+         System.out.println(slot3);
+         if(slot1 == slot2){
+            if(slot1 == slot3){
+               System.out.println("You won!");
+            }else{
+               System.out.println("You won half the money");
+            }
+         }else{
+            System.out.println("You lost");
+         }
+     }
+}
+
+
+   
+*/
