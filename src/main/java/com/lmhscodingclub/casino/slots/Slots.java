b6ab531221ba0dd23917a4c2c1cfe838d6e7027e
mshvartsberg <meggan567@gmail.com>
2021-10-26 15:33:58 -0400

    Add some things
    Co-authored-by: adelle7 <adelle7@users.noreply.github.com>

--- src/main/java/com/lmhscodingclub/casino/slots/Slots.java
+++ src/main/java/com/lmhscodingclub/casino/slots/Slots.java
@@ -1,21 +1,27 @@
 package com.lmhscodingclub.casino.slots;
 
+import java.util.Hashtable;
+
 public class Slots {
     Slots() {
 
     }
 
     void play() {
-        int slot1 = (int)(Math.random()*(20)+1);
-         int slot2 = (int)(Math.random()*(20)+1);
-         int slot3 = (int)(Math.random()*(20)+1);
+       Hashtable<Integer, Integer> payouts = new Hashtable<>();
+       payouts.put(10, 1000);
+       payouts.put()
+
+         int slot1 = (int)(Math.random()*(10)+1);
+         int slot2 = (int)(Math.random()*(10)+1);
+         int slot3 = (int)(Math.random()*(10)+1);
          System.out.println(slot1);
          System.out.println(slot2);
          System.out.println(slot3);
          if(slot1 == slot2){
-            if(slot1 == slot3){
+            if(slot1 == slot3 || slot2 == slot3){
                System.out.println("You won!");
-            }else{
+            }else{ 
                System.out.println("You won half the money");
             }
          }else{
