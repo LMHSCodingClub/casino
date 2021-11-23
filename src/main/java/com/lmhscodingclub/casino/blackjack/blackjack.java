de37ac272274d9709adcf7917d4216f4f119a8bd
Hivdum <59579911+hivdum@users.noreply.github.com>
2021-11-23 15:21:45 -0500

    Added things

--- src/main/java/com/lmhscodingclub/casino/blackjack/blackjack.java
+++ src/main/java/com/lmhscodingclub/casino/blackjack/blackjack.java
@@ -18,31 +18,63 @@ class blackjack{
           ArrayList<Card> hand = new ArrayList<Card>();
           ArrayList<Card> dealer = new ArrayList<Card>();
          
+         int total = 0;
+         int dealer_total = 0;
           
           //DO THE 11s ACE THING HERE, TOO
           
           for(int i = 0; i < 2; i++)
           {
             hand.add(new Card((int)(1 + (Math.random()*52))));
-            dealer.add(new Card((int)(1 + (Math.random()*52))));          
+            //hand.add(new Card(13));
+            dealer.add(new Card((int)(1 + (Math.random()*52))));
+            //dealer.add(new Card(13));
+                      
             if (hand.get(i).getValue() > 10)
             {
             //System.out.println(hand.get(i));
                hand.get(i).setValue(10);
             }
+            else if(hand.get(i).getValue() == 1)
+            {
+               hand.get(i).setValue(11);
+            }
+            
             if (dealer.get(i).getValue() > 10)
             {
             //System.out.println(hand.get(i));
                dealer.get(i).setValue(10);
             }
+            else if(dealer.get(i).getValue() == 1)
+            {
+               dealer.get(i).setValue(11);
+            }
+            
+            total += hand.get(i).getValue();
+            
+            dealer_total += dealer.get(i).getValue();
+            
           }
           
-          int total = hand.get(0).getValue() + hand.get(1).getValue();
-          int dealer_total = dealer.get(0).getValue() + dealer.get(1).getValue();
+          if(total > 21)
+          {
+            hand.get(0).setValue(1);
+            total -= 10;
+          }
+          if(dealer_total > 21)
+          {
+            dealer.get(0).setValue(1);
+            dealer_total -= 10;
+          }
+          
+          
+          
+        
           
           System.out.println("The Dealer has a " + dealer.get(0) + " showing.");
           
-          while (total < 22)
+          
+          while (total < 21)
           {
             
              System.out.print("Your current hand is a(n) " + hand.get(0));
@@ -74,6 +106,19 @@ class blackjack{
                   hand.get(hand.size()-1).setValue(11);
                }
                
+               if(total > 22)
+               {
+                for(int i = 0; i < hand.size(); i++)
+                {
+                   if(hand.get(i).getValue() == 11)
+                  {
+                  hand.get(i).setValue(1);
+                  total -= 10;
+                  break;
+                 }
+                } 
+               }
+               
              }
              else
              {
@@ -88,24 +133,16 @@ class blackjack{
           }
           else
           {
-            for(int i = 0; i < hand.size(); i++)
-             {
-               if(hand.get(i).getValue() == 11)
-                  {
-                  hand.get(i).setValue(1);
-                  total -= 10;
-                  continue;
-                  }
-             }
-            
-            total = -1;
-            System.out.println("You busted, buster!");   
+               total = -1;
+               System.out.println("You busted, buster!");  
           }
-          
+         
+        if(total != -1)
+        {
           
          boolean hit = true;
          
-          while(dealer_total < 22)
+          while(dealer_total < 21)
           {
             System.out.print("Dealer's hand is a(n) " + dealer.get(0));
              for(int i = 1; i < dealer.size(); i++)
@@ -166,7 +203,7 @@ class blackjack{
           if(dealer_total > 21)
            {
             System.out.println("The house busts. You win!");
-            dealer_total = -999;
+            dealer_total = 0;
            }
           else if(total > dealer_total)
           {
@@ -199,6 +236,11 @@ class blackjack{
             
           }
           
+        }
+        else
+        {
+        System.out.println("Better luck next time!");
+        }
                              
      }
 
